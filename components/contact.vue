96323ccd167e86b7ffd43593db933bb69d1c39b9
Jean-jacques AKAKPO <akakpo.jeanjacques@gmail.com>
2018-01-21 14:11:42 +0000

    Working on contact form

--- components/contact.vue
+++ components/contact.vue
@@ -3,6 +3,46 @@
     <curstep :append="curStep"></curstep>
 
     <div class="row-inner">
+      <div class="contact-content">
+        <header class="contact-header-text">{{ getContactHeaderText() }}</header>
+        <p class="contact-subheader-text">
+          we can talk about your amazing project around a coffee
+        </p>
+
+        <div class="contact-form-wrapper">
+          <form class="contact-form">
+            <div class="contact-form-group">
+              <input type="text" name="name" required value="" placeholder="full name" class="contact-form-field" autofocus="">
+            </div>
+            <div class="contact-form-group">
+              <input type="email" name="email" required value="" placeholder="email address" class="contact-form-field">
+            </div>
+            <div class="contact-form-group">
+              <textarea value="" name="message" placeholder="message" rows="6" class="contact-form-field" />
+            </div>
+            <div class="contact-form-group">
+              <input type="submit" name="send" value="Hello" class="contact-form-btn">
+
+              <div class="contact-social">
+                <ul class="social-icons">
+                  <li class="social-link">
+                    <a href="">
+                    </a>
+                  </li>
+                  <li class="social-link">
+                    <a href="">
+                    </a>
+                  </li>
+                  <li class="social-link">
+                    <a href="">
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -20,7 +60,11 @@
       }
     },
     props: {},
-    methods: {},
+    methods: {
+      getContactHeaderText () {
+        return 'get in touch with me'
+      }
+    },
     mounted () {}
   }
 </script>
