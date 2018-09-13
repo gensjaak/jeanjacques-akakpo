ffdbd7154509d6fae76698aaac34fa573beb5105
Jean-jacques AKAKPO <akakpo.jeanjacques@gmail.com>
2018-09-13 17:36:04 +0200

    Update page description

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Description' }
+      { hid: 'description', name: 'description', content: 'This is just another portfolio. Period.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
